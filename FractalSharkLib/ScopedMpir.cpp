4ddbf72d8791db2760c1ae4bfd32e5f0461e934f
Matthew Renzelmann <mattp364@renzelmann.com>
2024-03-02 11:15:04 -0800

    Bumped down, verified reference orbit reuse still works

--- FractalSharkLib/ScopedMpir.cpp
+++ FractalSharkLib/ScopedMpir.cpp
@@ -4,9 +4,12 @@
 #include "HighPrecision.h"
 
 thread_local uint8_t ScopedMPIRAllocators::Allocated[NumBlocks][BytesPerBlock];
-thread_local size_t ScopedMPIRAllocators::AllocatedSize[NumBlocks];
+thread_local uint8_t *ScopedMPIRAllocators::AllocatedEnd[NumBlocks];
 thread_local size_t ScopedMPIRAllocators::AllocatedIndex = 0;
 thread_local size_t ScopedMPIRAllocators::AllocationsAndFrees[NumBlocks];
+std::atomic<size_t> ScopedMPIRAllocators::MaxAllocatedDebug = 0;
+
+static constexpr bool DebugInstrument = false;
 
 ScopedMPIRAllocators::ScopedMPIRAllocators() :
     ExistingMalloc{},
@@ -39,66 +42,109 @@ void ScopedMPIRAllocators::InitScopedAllocators() {
 
 void ScopedMPIRAllocators::InitTls() {
     memset(Allocated, 0, sizeof(Allocated));
-    memset(AllocatedSize, 0, sizeof(AllocatedSize));
-    AllocatedIndex = 0;
     memset(AllocationsAndFrees, 0, sizeof(AllocationsAndFrees));
+    AllocatedIndex = 0;
+
+    for (size_t i = 0; i < NumBlocks; i++) {
+        AllocatedEnd[i] = &Allocated[i][BytesPerBlock];
+    }
 }
 
 void ScopedMPIRAllocators::ShutdownTls() {
     // If there are any allocations left, we have a memory leak.
-    for (size_t i = 0; i < NumBlocks; i++) {
-        if (AllocationsAndFrees[i] != 0) {
-            DebugBreak();
+    if constexpr (DebugInstrument) {
+        for (size_t i = 0; i < NumBlocks; i++) {
+            if (AllocationsAndFrees[i] != 0) {
+                DebugBreak();
+            }
         }
     }
 }
 
 void* ScopedMPIRAllocators::NewMalloc(size_t size) {
-    const auto alignedSize = (size + 63) & ~63;
-    size_t indexToAllocateAt = AllocatedSize[AllocatedIndex];
+    uint8_t *newPtr = AllocatedEnd[AllocatedIndex] - size;
 
-    if (AllocatedSize[AllocatedIndex] + alignedSize >= BytesPerBlock) {
+    // Round down to requested alignment:
+    newPtr = (uint8_t*)((uintptr_t)newPtr & ~63);
+
+    if (newPtr >= &Allocated[AllocatedIndex][0]) {
+        AllocationsAndFrees[AllocatedIndex]++;
+        AllocatedEnd[AllocatedIndex] = newPtr;
+
+        if constexpr (DebugInstrument) {
+            // This all is a bit overkill but at least it's guaranteed to work.
+            // Keeps track of maximum allocations required.
+            for (;;) {
+                auto oldVal = MaxAllocatedDebug.load(std::memory_order_seq_cst);
+                if (AllocationsAndFrees[AllocatedIndex] > oldVal) {
+                    auto result = MaxAllocatedDebug.compare_exchange_weak(
+                        oldVal,
+                        AllocationsAndFrees[AllocatedIndex],
+                        std::memory_order_seq_cst);
+
+                    if (result) {
+                        break;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        return newPtr;
+    } else {
         // Find the next block with no allocations:
+        size_t InitIndex = AllocatedIndex;
         for (;;) {
             AllocatedIndex = (AllocatedIndex + 1) % NumBlocks;
             if (AllocationsAndFrees[AllocatedIndex] == 0) {
-                AllocatedSize[AllocatedIndex] = 0;
-                indexToAllocateAt = 0;
-                break;
+                newPtr = &Allocated[AllocatedIndex][BytesPerBlock] - size;
+                newPtr = (uint8_t*)((uintptr_t)newPtr & ~63);
+                AllocationsAndFrees[AllocatedIndex]++;
+                AllocatedEnd[AllocatedIndex] = newPtr;
+                return newPtr;
+            }
+
+            if constexpr (DebugInstrument) {
+                if (AllocatedIndex == InitIndex) {
+                    DebugBreak();
+                }
             }
         }
     }
-    else {
-        indexToAllocateAt = AllocatedSize[AllocatedIndex];
-    }
-
-    auto ret = &Allocated[AllocatedIndex][AllocatedSize[AllocatedIndex]];
-
-    // Increment AllocatedSize by 64-byte aligned size:
-    AllocatedSize[AllocatedIndex] += alignedSize;
-    AllocationsAndFrees[AllocatedIndex]++;
-
-    return ret;
 }
 
 void* ScopedMPIRAllocators::NewRealloc(void* ptr, size_t old_size, size_t new_size) {
     // This one is like new_malloc, but copy in the prior data.
-    auto ret = NewMalloc(new_size);
-    auto minimum_size = std::min(old_size, new_size);
-    memcpy(ret, ptr, minimum_size);
-    return ret;
+    //auto ret = NewMalloc(new_size);
+    //auto minimum_size = std::min(old_size, new_size);
+    //memcpy(ret, ptr, minimum_size);
+
+    if constexpr (DebugInstrument) {
+        DebugBreak();
+    }
+    return nullptr;
 }
 
 void ScopedMPIRAllocators::NewFree(void* ptr, size_t size) {
-    const auto alignedSize = (size + 63) & ~63;
-
     // Find offset relative to base of Allocated:
     const uint64_t offset = (uint8_t*)ptr - (uint8_t*)&Allocated[0][0];
 
     // Find index of Allocated:
     const size_t index = offset / BytesPerBlock;
 
-    AllocationsAndFrees[index]--;
+    if constexpr (DebugInstrument) {
+        if (index >= NumBlocks) {
+            DebugBreak();
+        }
+
+        if (AllocationsAndFrees[index] == 0) {
+            DebugBreak();
+        }
+    }
+
+    --AllocationsAndFrees[index];
 }
 
 ScopedMPIRPrecision::ScopedMPIRPrecision(unsigned digits10) : saved_digits10(HighPrecision::thread_default_precision())
--- FractalSharkLib/ScopedMpir.h
+++ FractalSharkLib/ScopedMpir.h
@@ -4,11 +4,8 @@
 #include <boost/multiprecision/gmp.hpp>
 #include <boost/multiprecision/number.hpp>
 
+// Bump allocator that bumps downwards
 struct ScopedMPIRAllocators {
-    void* (*ExistingMalloc) (size_t);
-    void* (*ExistingRealloc) (void*, size_t, size_t);
-    void (*ExistingFree) (void*, size_t);
-
     ScopedMPIRAllocators();
     ~ScopedMPIRAllocators();
 
@@ -17,15 +14,20 @@ struct ScopedMPIRAllocators {
     static void InitTls();
     static void ShutdownTls();
 
-    static constexpr size_t NumBlocks = 4;
+private:
+    void* (*ExistingMalloc) (size_t);
+    void* (*ExistingRealloc) (void*, size_t, size_t);
+    void (*ExistingFree) (void*, size_t);
+
+    static constexpr size_t NumBlocks = 2;
     static constexpr size_t BytesPerBlock = 1 * 1024 * 1024;
 
     thread_local static uint8_t Allocated[NumBlocks][BytesPerBlock];
-    thread_local static size_t AllocatedSize[NumBlocks];
+    thread_local static uint8_t* AllocatedEnd[NumBlocks];
     thread_local static size_t AllocatedIndex;
     thread_local static size_t AllocationsAndFrees[NumBlocks];
+    static std::atomic<size_t> MaxAllocatedDebug;
 
-private:
     static void* NewMalloc(size_t size);
     static void* NewRealloc(void* ptr, size_t old_size, size_t new_size);
     static void NewFree(void* ptr, size_t size);

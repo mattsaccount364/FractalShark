33b118cb739a50e4d5c715290b6db2c819b073dc
Matthew Renzelmann <mattp364@renzelmann.com>
2026-07-25 14:47:50 -0700

    Disable debug instrumentation, best result on v2 ref is 4.5s now on view5 perf.

--- HpSharkFloatLib/HpSharkTestConfig.h
+++ HpSharkFloatLib/HpSharkTestConfig.h
@@ -35,11 +35,11 @@ namespace HpShark {
 static constexpr bool TestGpu = true;
 static constexpr bool TestInfiniteCorrectness = true;
 static constexpr auto TestForceSameSign = false;
-static constexpr bool TestMPIRImpl = Debug;
+static constexpr bool TestMPIRImpl = false;
 
 // True to compare against the full host-side reference implementation, false is MPIR only
 // False is useful to speed up e.g. testing many cases fast but gives poor diagnostic results.
-static constexpr bool TestReferenceImpl = true;
+static constexpr bool TestReferenceImpl = false;
 
 } // namespace HpShark
 
